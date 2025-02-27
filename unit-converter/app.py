6b85ab96bcf2943444cfe33d421307926b86bcd5
kashifaliawan <kashifalismitofficial@gmail.com>
2025-02-27 11:38:56 +0500

    convert weight

--- unit-converter/app.py
+++ unit-converter/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 # Define conversion functions
 def convert_length(value, from_unit, to_unit):
     length_conversions = {
@@ -12,4 +13,17 @@ def convert_length(value, from_unit, to_unit):
         "yard": 0.9144,
         "mile": 1609.34,
     }
-   
+    return value * (length_conversions[from_unit] / length_conversions[to_unit])
+
+
+def convert_weight(value, from_unit, to_unit):
+    weight_conversions = {
+        "gram": 1.0,
+        "kilogram": 1000.0,
+        "milligram": 0.001,
+        "pound": 453.592,
+        "ounce": 28.3495,
+    }
+    return value * (weight_conversions[from_unit] / weight_conversions[to_unit])
+
+
