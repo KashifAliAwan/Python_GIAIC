131fba72810e0f9b0b3889ae2cc956b59da9211b
kashifaliawan <kashifalismitofficial@gmail.com>
2025-03-08 16:39:15 +0500

    int

---
+++ practice_python/Lesson01.py
@@ -0,0 +1,8 @@
+# Numeric Intiger
+numeric_int : int = 20
+numeric_float : float = 100.5
+numeric_complex : complex = 3j
+
+print(type(numeric_int),"Intiger = ", numeric_int)
+print(type(numeric_float),"Float = ",numeric_float)
+print(type(numeric_complex),"Complex Number = ",numeric_complex)
