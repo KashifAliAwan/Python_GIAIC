e2f6a059b6c51fbc7e2576ad5f1b0ebbbf68b488
kashifaliawan <kashifalismitofficial@gmail.com>
2025-03-08 16:39:51 +0500

    dictionary

--- practice_python/Lesson01.py
+++ practice_python/Lesson01.py
@@ -6,3 +6,18 @@ numeric_complex : complex = 3j
 print(type(numeric_int),"Intiger = ", numeric_int)
 print(type(numeric_float),"Float = ",numeric_float)
 print(type(numeric_complex),"Complex Number = ",numeric_complex)
+
+
+
+
+# Dictionary (Mapping Type)
+dictionary : dict = {"Name":"Kashif Ali" , "age" : 20 , "Language" : "Hindko" , "Role":"Developer"}
+# Add in Dictionary
+dictionary["Institute"] = "GIAIC"
+# Adding a New Key-Value Pair
+dictionary["City"] = "Karachi"
+# Removing a Key-Value Pair
+del dictionary["City"]
+# Using Dictionary Methods to Modify
+dictionary.update({"Role":"Full Stack Developer"})
+print(type(dictionary), "Dictionary" , dictionary )
