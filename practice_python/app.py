43fe6d445b13a087aac4c56d781a5fe67b3dbb76
kashifaliawan <kashifalismitofficial@gmail.com>
2025-03-08 16:35:10 +0500

    practice

--- practice_python/app.py
+++ practice_python/app.py
@@ -1,1 +1,9 @@
-print('kashifali')
+# Set 
+set_variable : set = {2,3,4,5,5,6,7,8,9,10,1}
+print(type(set_variable), "Set = " , set_variable)
+#List
+list_variable : list = [2,3,4,5,5,6,7,8,9,10,1]
+print(type(list_variable), "List =",list_variable)
+# Froozenset
+frozenset_variable : frozenset = frozenset({"kashif ali","ali","kashif ali",5,2,55,55,8,6,15,10,1})
+print(type(frozenset_variable),"Frozen Set = ",frozenset_variable)
