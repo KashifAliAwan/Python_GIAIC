ff116ebda3ea1d2ce1a142677f3704e1b74091d8
kashifaliawan <kashifalismitofficial@gmail.com>
2025-03-10 17:00:31 +0500

    Examples are there

--- practice_python/lesson02.py
+++ practice_python/lesson02.py
@@ -1,1 +1,40 @@
 # Operands And Operator
+floor_division = 16 // 3 # 2
+print(floor_division)
+
+
+modul_division = 16 % 3 # 0
+print(modul_division)
+
+
+modul_division = 16 / 3 # 0
+print(modul_division)
+
+
+
+# Arithmetic Operators
+
+a: int = 10
+b: int = 3
+print("a + b  = ", a + b)   #13
+print("a - b  = ", a - b)   #7
+print("a * b  = ", a * b)   #30
+print("a / b  = ", a / b)   #3.3333
+print("a // b = ", a // b)  #3
+print("a % b  = ", a % b)   #1
+print("a % b  = ", a ** b)  #1000
+
+
+# Comparison Operators
+
+a: int = 8
+b: int = 12
+
+print("a == b = False ", a == b)  # Equal
+print("a != b = True ", a != b)  # Not equal
+print("a > b  = False ", a > b)   # Greater than
+print("a < b  = True", a < b)   # Less than
+print("a >= b = False", a >= b)  # Greater than or equal
+print("a <= b = True", a <= b)  # Less than or equal
+
+#
