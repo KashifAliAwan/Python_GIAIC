085e4f72276e8e1ce13291401477ac2a4ce0847c
kashifaliawan <kashifalismitofficial@gmail.com>
2025-03-11 16:26:18 +0500

    logical operator simple examples

--- practice_python/lesson02.py
+++ practice_python/lesson02.py
@@ -39,14 +39,89 @@
 
 #Hard Example
 
-a: int = 10
-b: int = 5
-c: int = 20
+# a: int = 10
+# b: int = 5
+# c: int = 20
 
-# Complex comparisons with arithmetic operations
-print("(a + b) * 2 == c = False", (a + b) * 2 == c)          # Check if (10 + 5) * 2 equals 20
-print("a ** 2 > b * c =   False", a ** 2 > b * c)              # Check if 10^2 is greater than 5 * 20
-print("(c % a) + b != a = True", (c % a) + b != a)          # Check if (20 % 10) + 5 is not equal to 10
-print("(a // b) * c <= a + b + c =  False", (a // b) * c <= a + b + c)  # Check if (10 // 5) * 20 <= 10 + 5 + 20 <><> 40
-print("a * (b + c) >= c ** 2  False= ", a * (b + c) >= c ** 2) # Check if 10 * (5 + 20) >= 20^2
-print("(a + c) // b == a = False", (a + c) // b == a)        # Check if (10 + 20) // 5 equals 10
+# # Complex comparisons with arithmetic operations
+# print("(a + b) * 2 == c = False", (a + b) * 2 == c)          # Check if (10 + 5) * 2 equals 20
+# print("a ** 2 > b * c =   False", a ** 2 > b * c)              # Check if 10^2 is greater than 5 * 20
+# print("(c % a) + b != a = True", (c % a) + b != a)          # Check if (20 % 10) + 5 is not equal to 10
+# print("(a // b) * c <= a + b + c =  False", (a // b) * c <= a + b + c)  # Check if (10 // 5) * 20 <= 10 + 5 + 20 <><> 40
+# print("a * (b + c) >= c ** 2  False= ", a * (b + c) >= c ** 2) # Check if 10 * (5 + 20) >= 20^2
+# print("(a + c) // b == a = False", (a + c) // b == a)        # Check if (10 + 20) // 5 equals 10
+
+
+
+
+# Example 1
+a: bool = True
+b: bool = True
+print("a and b = True", a and b)
+print("a or b  = True", a or b)
+print("not a   = False", not a)
+print("Example No 01 <<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>")
+
+# Example 2
+c: bool = False
+d: bool = False
+print("c and d = False", c and d)
+print("c or d  = False", c or d)
+print("not c   = True", not c)
+
+# Example 3
+e: bool = True
+f: bool = False
+print("e and f = False", e and f)
+print("e or f  = True", e or f)
+print("not e   = False", not e)
+
+# Example 4
+g: bool = False
+h: bool = True
+print("g and h = False", g and h)
+print("g or h  = True", g or h)
+print("not g   = True", not g)
+
+# Example 5
+i: bool = True
+j: bool = True
+print("i and j = T", i and j)
+print("i or j  = T", i or j)
+print("not i   = F", not i)
+
+# Example 6
+k: bool = False
+l: bool = True
+print("k and l = F", k and l)
+print("k or l  = T", k or l)
+print("not k   = T", not k)
+
+# Example 7
+m: bool = True
+n: bool = False
+print("m and n = F", m and n)
+print("m or n  = T", m or n)
+print("not m   = F", not m)
+
+# Example 8
+o: bool = False
+p: bool = False
+print("o and p = F", o and p)
+print("o or p  = F", o or p)
+print("not o   = T", not o)
+
+# Example 9
+q: bool = True
+r: bool = True
+print("q and r = T", q and r)
+print("q or r  = T", q or r)
+print("not q   = F", not q)
+
+# Example 10
+s: bool = False
+t: bool = True
+print("s and t = F", s and t)
+print("s or t  = T", s or t)
+print("not s   = T", not s)
+
